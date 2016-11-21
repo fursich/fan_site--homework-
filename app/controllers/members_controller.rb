1f22910580cc6f0b63051c1c40ae172cb42abffb
Koji <kozzzzy@gmail.com>
2016-11-21 02:30:51 +0000

    機能追加 new/create機能を実装　テスト前

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -7,4 +7,20 @@ class MembersController < ApplicationController
         @member = Member.find(params[:id])
     end
 
+    def new
+        @member = Member.new
+    end
+
+    def create
+        @member = Member.new(member_params)
+        @member.save
+        
+        redirect_to '/members'
+    end
+    
+    private
+    def member_params
+        params.require(:member).permit(:family_name,:given_name,:img_url)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get 'members' => 'members#index'
   get 'members/:id' => 'members#show'
+  get 'members/new' => 'members#new'
+  post 'members' => 'members#create'
 
 end
